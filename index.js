7f4ad2afd2cb425a758c3527b9282035dafc461d
Othman Mashaab <red.fantastic@hotmail.com>
2018-08-09 01:08:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -40,4 +40,5 @@ class Fish {
 
 var caldwell = new Dog("caldwell", "toy poodle", "4 months");
       var puff = new Cat("Professor Puff", "Maine Coon", "6 years");
+      var sugarDaddy = new Ferret("Sugar Daddy", "2 years", "raw meat");
 
