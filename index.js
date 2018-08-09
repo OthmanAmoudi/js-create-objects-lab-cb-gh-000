e34c70a7782035501a950fb9d585fdb611c1922f
Othman Mashaab <red.fantastic@hotmail.com>
2018-08-09 01:00:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,40 @@
 'use strict';
 class Dog {
-  constructor(){
+  constructor(name,breed,age){
     this.name = name;
+    this.breed = breed;
+    this.age = age;
+  }
+}
+
+class Cat {
+    constructor(name,breed,age){
     this.name = name;
+    this.breed = breed;
+    this.age = age;
+  }
+}
+
+class Ferret {
+    constructor(name,breed,age){
     this.name = name;
+    this.breed = breed;
+    this.age = age;
+  }
+}
+
+class Cat {
+    constructor(name,breed,age){
     this.name = name;
+    this.breed = breed;
+    this.age = age;
+  }
+}
+
+class Cat {
+    constructor(name,breed,age){
+    this.name = name;
+    this.breed = breed;
+    this.age = age;
   }
 }
