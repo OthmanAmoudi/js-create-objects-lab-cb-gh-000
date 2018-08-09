258efda7bef0058fdafe81b4e5354dd91278adae
Othman Mashaab <red.fantastic@hotmail.com>
2018-08-09 01:11:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 'use strict';
+function Dog (name,breed,age){
+    this.name = name;
+    this.breed = breed;
+    this.age = age;
+  }
+
+
+function Cat (name,breed,age){
+    this.name = name;
+    this.breed = breed;
+    this.age = age;
+  }
+
+
+function Ferret (name,age,foodRequirements){
+    this.name = name;
+    this.age = age;
+    this.foodRequirements = foodRequirements;
+  }
+
+
+function Bird (name,breed){
+    this.name = name;
+    this.breed = breed;
+  }
+
+
+function Fish (name,species,waterConditions){
+    this.name = name;
+    this.species = species;
+    this.waterConditions = waterConditions;
+  }
+
+
+      // var caldwell = new Dog("caldwell", "toy poodle", "4 months");
+      // var puff = new Cat("Professor Puff", "Maine Coon", "6 years");
+      // var sugarDaddy = new Ferret("Sugar Daddy", "2 years", "raw meat");
+      // var darla = new Bird("Darla", "cockatoo");
+      // var pinocchio = new Fish("Pinocchio", "parrot fish", "salt water");
+
